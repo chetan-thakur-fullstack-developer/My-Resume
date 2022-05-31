8b6a1d298ff27f3be6564f59af24edce71bd1258
Thakur, Chetan (ct7477) <chetanthakur_98@yahoo.com>
2022-05-30 21:16:33 -0500

    Updates

---
+++ precache-manifest.4f33400c78b72ab67832914c80d5d3a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abc1d29b60edf8255778a94d8edeff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "342c57277ee973fa4552",
+    "url": "/static/css/2.198f404e.chunk.css"
+  },
+  {
+    "revision": "cc8269ade655c1c81aa5",
+    "url": "/static/css/main.c3aac0e0.chunk.css"
+  },
+  {
+    "revision": "342c57277ee973fa4552",
+    "url": "/static/js/2.59f99255.chunk.js"
+  },
+  {
+    "revision": "6703d21253ec1478be1545a1dd9e80fc",
+    "url": "/static/js/2.59f99255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006310b866459960ce56",
+    "url": "/static/js/3.965548ea.chunk.js"
+  },
+  {
+    "revision": "cc8269ade655c1c81aa5",
+    "url": "/static/js/main.c46b0fb8.chunk.js"
+  },
+  {
+    "revision": "f596c25a7e3cc44794f9",
+    "url": "/static/js/runtime-main.14ffe102.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
